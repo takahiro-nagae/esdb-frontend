b92bd47e7380be6c99eb95040bce1be1c8a3fa72
tnagae627 <>
2022-02-04 22:29:57 +0900

    [update]cssのリファクタ

--- src/search/searchListHeadPc.tsx
+++ src/search/searchListHeadPc.tsx
@@ -4,18 +4,25 @@ import { css } from '@emotion/react';
 
 /**
  * PC用の検索一覧ヘッダー
- * @param: tableHeader ヘッダ行のcss
  */
-export const SearchListHeadPc = (props: {tableHeader: any}) => {
+export const SearchListHeadPc = () => {
+
+    /** テーブルヘッダー */
+    const tableHeader = css ({
+        backgroundColor: '#0854a3',
+        color: '#fff',
+        border: 'none'
+    });
+
     return(
         <TableRow>
-            <TableCell css={props.tableHeader}>名称</TableCell>
-            <TableCell css={props.tableHeader}>位置</TableCell>
-            <TableCell css={props.tableHeader}>ランク</TableCell>
-            <TableCell css={props.tableHeader}>対象</TableCell>
-            <TableCell css={props.tableHeader}>値</TableCell>
-            <TableCell css={props.tableHeader}>効果</TableCell>
-            <TableCell css={props.tableHeader}>入手先</TableCell>
+            <TableCell css={tableHeader}>名称</TableCell>
+            <TableCell css={tableHeader}>位置</TableCell>
+            <TableCell css={tableHeader}>ランク</TableCell>
+            <TableCell css={tableHeader}>対象</TableCell>
+            <TableCell css={tableHeader}>値</TableCell>
+            <TableCell css={tableHeader}>効果</TableCell>
+            <TableCell css={tableHeader}>入手先</TableCell>
         </TableRow>
     );
 }
