b83de0b84a6ab2bd5fc898d18082edd73717499c
takahiro-nagae <tnagae627@gmail.com>
2022-02-27 23:44:09 +0900

    [update]スマホ版を動的に表示できるように修正

--- src/search/positionFunction.tsx
+++ src/search/positionFunction.tsx
@@ -6,7 +6,7 @@ import { css } from "@emotion/react";
  * @param positionCode 位置コード
  * @returns 位置コードの値による色
  */
-export default function positionColor (positionCode: string) {
+export function positionColor (positionCode: string) {
     switch(positionCode) {
         case '1':
             // 接頭
@@ -22,5 +22,23 @@ export default function positionColor (positionCode: string) {
             return css({
                 color: '#fff'
             });
+    }
+}
+
+/**
+ * 位置コードの値で名称を取得する
+ * @param positionCode 位置コード
+ * @returns 位置コードの名称
+ */
+export function positionName(positionCode: string) {
+    switch(positionCode) {
+        case '1':
+            // 接頭
+            return '接頭(prefix)';
+        case '2':
+            // 接尾
+            return '接尾(suffix)';
+        default:
+            return '';
     }
 }
