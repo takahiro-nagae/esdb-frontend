68d372356091bc254e377b50471e08a657a1b523
tnagae627 <>
2022-03-08 15:55:58 +0900

    [update]ローディングを作成

--- src/search/searchList.tsx
+++ src/search/searchList.tsx
@@ -1,9 +1,10 @@
 /** 標準ライブラリ */
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 /** サードパーティーライブラリ */
 /** @jsxImportSource @emotion/react */
+import ReactLoading from 'react-loading';
+import axios from 'axios';
 import { css } from '@emotion/react';
 import { useSearchParams } from 'react-router-dom';
 import MediaQuery from "react-responsive";
@@ -24,8 +25,8 @@ import { EnchantCard } from './enchantCard';
  */
 export const SearchList = (props: {maxWidth: any, breakPoint: number}) => {
 
-    /** ヒットしました文字列 */
-    const hit = css({
+    /** 検索結果の文字列 */
+    const result = css({
         fontWeight: 'bold',
         color: '#fff'
     });
@@ -43,8 +44,15 @@ export const SearchList = (props: {maxWidth: any, breakPoint: number}) => {
         border: 'none'
     });
 
+    /** スマホの横幅指定 */
     const spWidth = css ({
        width: '100%'
+    });
+
+    /** ローディングや検索結果なしの表示 */
+    const verticalCenter = css ({
+        position: 'absolute',
+        top: '50%'
     });
 
     /** 遷移元からのデータ */
@@ -59,6 +67,8 @@ export const SearchList = (props: {maxWidth: any, breakPoint: number}) => {
     const [count, setcount] = useState(0);
     /** 値表示のフラグ */
     const [valFlag, setValFlag] = useState(false);
+    /** ローディングフラグ */
+    const [loadingFlag, setLoadingFlag] = useState(false);
 
 
     // ********************
@@ -77,7 +87,11 @@ export const SearchList = (props: {maxWidth: any, breakPoint: number}) => {
                 // 件数
                 setcount(res.data.length);
                 // 値の表示フラグ
-                setValFlag(res.data[0].disp_val != undefined)
+                if(count > 0) {
+                    setValFlag(res.data[0].disp_val != undefined)
+                }
+                // ローディング完了
+                setLoadingFlag(true);
             }
         }).catch((error) => {
             console.log(error)
@@ -90,45 +104,57 @@ export const SearchList = (props: {maxWidth: any, breakPoint: number}) => {
 
     return(
         <Box sx={{ mt: 3}}>
+            <Grid container alignItems='center' direction='column' css={verticalCenter}>
+                { !loadingFlag && <ReactLoading type="spin" /> }
+                { loadingFlag && count == 0 &&
+                    <>
+                        <p css={result}>検索結果は0件です</p>
+                    </>
+                }
+            </Grid>
             <Grid container alignItems='center' direction='column'>
-                <p css={hit}><span css={hitCount}>{count}</span>件ヒットしました</p>
-                <MediaQuery query={minQuery}>
-                    <Grid item xs={11}>
-                        <Box sx={{ p: 1}}>
-                            <TableContainer sx={{ maxHeight: 800 }}>
-                                <Table stickyHeader aria-label="sticky table">
-                                    {/* ヘッダー */}
-                                    <TableHead>
-                                         <TableRow>
-                                            <TableCell css={tableHeader}>名称</TableCell>
-                                            <TableCell css={tableHeader}>位置</TableCell>
-                                            <TableCell css={tableHeader}>ランク</TableCell>
-                                            <TableCell css={tableHeader}>対象</TableCell>
-                                            { valFlag && <TableCell css={tableHeader}>値</TableCell> }
-                                            <TableCell css={tableHeader}>効果</TableCell>
-                                            <TableCell css={tableHeader}>入手先</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    {/* ボディ */}
-                                    <TableBody>
-                                        {enchantList.map(enchant => (
-                                            <SearchListBody enchant={enchant} valFlg={valFlag} key={enchant.enchant_id} />
-                                        ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </Box>
-                    </Grid>
-                </MediaQuery>
-                <MediaQuery query={maxQuery}>
-                    <Grid item xs={12} css={spWidth}>
-                        <Box sx={{ p: 1}}>
-                            {enchantList.map(enchant => (
-                                <EnchantCard enchant={enchant} valFlag={valFlag} key={enchant.enchant_id} />
-                            ))}
-                        </Box>
-                    </Grid>
-                </MediaQuery>
+                { loadingFlag && count > 0 &&
+                    <>
+                        <p css={result}><span css={hitCount}>{count}</span>件ヒットしました</p>
+                        <MediaQuery query={minQuery}>
+                            <Grid item xs={11}>
+                                <Box sx={{ p: 1}}>
+                                    <TableContainer sx={{ maxHeight: 800 }}>
+                                        <Table stickyHeader aria-label="sticky table">
+                                            {/* ヘッダー */}
+                                            <TableHead>
+                                                    <TableRow>
+                                                    <TableCell css={tableHeader}>名称</TableCell>
+                                                    <TableCell css={tableHeader}>位置</TableCell>
+                                                    <TableCell css={tableHeader}>ランク</TableCell>
+                                                    <TableCell css={tableHeader}>対象</TableCell>
+                                                    { valFlag && <TableCell css={tableHeader}>値</TableCell> }
+                                                    <TableCell css={tableHeader}>効果</TableCell>
+                                                    <TableCell css={tableHeader}>入手先</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            {/* ボディ */}
+                                            <TableBody>
+                                                {enchantList.map(enchant => (
+                                                    <SearchListBody enchant={enchant} valFlg={valFlag} key={enchant.enchant_id} />
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Box>
+                            </Grid>
+                        </MediaQuery>
+                        <MediaQuery query={maxQuery}>
+                            <Grid item xs={12} css={spWidth}>
+                                <Box sx={{ p: 1}}>
+                                    {enchantList.map(enchant => (
+                                        <EnchantCard enchant={enchant} valFlag={valFlag} key={enchant.enchant_id} />
+                                    ))}
+                                </Box>
+                            </Grid>
+                        </MediaQuery>
+                    </>
+                }
             </Grid>
         </Box>
     );
