90c0988843a3907e21c267f11987e63b6b94a62a
tnagae627 <>
2022-03-10 14:29:26 +0900

    [update]ランクモーダルを動的に取得できるように変更

--- src/search/rankModal.tsx
+++ src/search/rankModal.tsx
@@ -1,9 +1,10 @@
 /** 標準ライブラリ */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 /** サードパーティーライブラリ */
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import axios from "axios";
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
@@ -16,6 +17,21 @@ export const RankModal = (props: {rank: string}) => {
 
     /** ランクオープンのフラグ */
     const [open, setOpen] = useState(false);
+    /** ランクの取得データ */
+    const [rankData, setRankData] = useState({
+        rank: '-',
+        normal_rate: '-',
+        elite_rate: '-',
+        elf_rate: '-',
+        ancient_rate: '-',
+        rare_holy_rate: '-',
+        normal_rate_thu: '-',
+        elite_rate_thu: '-',
+        elf_rate_thu: '-',
+        ancient_rate_thu: '-',
+        rare_holy_rate_thu: '-'
+    });
+
     /**  モーダルオープン処理 */
     const handleOpen = () => {
       setOpen(true);
@@ -54,6 +70,22 @@ export const RankModal = (props: {rank: string}) => {
         backgroundColor: '#0854a3',
     });
 
+    // ********************
+    // 初期表示
+    // ********************
+    useEffect(() => {
+
+        axios.get('https://wd5zeazzd9.execute-api.ap-northeast-1.amazonaws.com/Prod/rank/' + props.rank)
+        .then((res) => {
+            if(res.data != undefined) {
+                // ランクデータをセット
+                setRankData(res.data);
+            }
+        }).catch((error) => {
+            console.log(error)
+        });
+    }, []);
+
     return(
         <>
             <Button onClick={handleOpen}>{props.rank}</Button>
@@ -64,7 +96,7 @@ export const RankModal = (props: {rank: string}) => {
                 aria-describedby="modal-modal-description"
             >
             <Box css={modalBox}>
-                <p css={description}>ランク：<span css={dispRank}>{props.rank}</span></p>
+                <p css={description}>ランク：<span css={dispRank}>{rankData.rank}</span></p>
                 <p css={description}>INT:200時　単位：%</p>
                 <Table>
                     <TableHead>
@@ -80,19 +112,19 @@ export const RankModal = (props: {rank: string}) => {
                     <TableBody>
                         <TableRow>
                             <TableCell>木曜日以外</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
+                            <TableCell>{rankData.normal_rate}</TableCell>
+                            <TableCell>{rankData.elite_rate}</TableCell>
+                            <TableCell>{rankData.elf_rate}</TableCell>
+                            <TableCell>{rankData.ancient_rate}</TableCell>
+                            <TableCell>{rankData.rare_holy_rate}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>木曜日</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
-                            <TableCell>90</TableCell>
+                            <TableCell>{rankData.normal_rate_thu}</TableCell>
+                            <TableCell>{rankData.elite_rate_thu}</TableCell>
+                            <TableCell>{rankData.elf_rate_thu}</TableCell>
+                            <TableCell>{rankData.ancient_rate_thu}</TableCell>
+                            <TableCell>{rankData.rare_holy_rate_thu}</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
