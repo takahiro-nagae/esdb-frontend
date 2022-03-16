bedc95728fa6420e8b4db71ff5615b4ee235a17e
takahiro-nagae <tnagae627@gmail.com>
2022-03-16 10:24:10 +0900

    [update]ページネーションを外部化

---
+++ src/search/pc/pagination.tsx
@@ -0,0 +1,39 @@
+/** 標準ライブラリ */
+import { useState } from 'react';
+
+/** サードパーティーライブラリ */
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { TablePagination } from '@mui/material';
+
+/**
+ *　検索結果のページネーション
+ */
+export const Pagination = (props: {count: number, page: number, rowsPerPage: number
+    , handleChangePage: any, handleChangeRowsPerPage: any}) => {
+
+    /** ページネーション */
+    const pagenation = css ({
+        backgroundColor: '#3C3B40',
+        color: '#ccc',
+        borderTop: '1px solid rgba(81, 81, 81, 1)',
+        borderBottom: '1px solid rgba(81, 81, 81, 1)',
+        '.css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon' : {
+            color: '#fff'
+        }
+    });
+
+    return(
+        <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={props.count}
+            rowsPerPage={props.rowsPerPage}
+            page={props.page}
+            onPageChange={props.handleChangePage}
+            onRowsPerPageChange={props.handleChangeRowsPerPage}
+            css={pagenation}
+        />
+    )
+
+}
