bd14b73796037b5cc756f3e5b63cfcacc6ebc9b9
takahiro-nagae <tnagae627@gmail.com>
2022-05-01 18:58:17 +0900

    ホームのコメント系リファクタ

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,15 +1,22 @@
-/** ローカルライブラリ */
 import { MultiplexAd } from '../adsense/multiplexAd';
 import { SearchFormContainer } from '../form/searchFormContainer';
 
 /**
- * トップ画面
+ * トップ画面コンポーネント
+ * @param props { any, any } // TODO: anyの型付けを変更
+ * @returns Home　{ JSX.Element }
  */
-export const Home = (props: {mq: any, maxWidth: any}) => {
+export const Home = (props: {
+    mq: any,
+    maxWidth: any
+}) => {
     return(
         <>
-            <SearchFormContainer mq={props.mq} maxWidth={props.maxWidth} />
-              <MultiplexAd />
+            <SearchFormContainer
+                maxWidth={props.maxWidth}
+                mq={props.mq}
+            />
+            <MultiplexAd />
         </>
     )
 }
