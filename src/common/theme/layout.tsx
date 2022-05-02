1434aee040aac422c6f8b5c12277536a1b9cfd3a
kumario <kumalog@jackumagic.com>
2022-05-02 23:42:46 +0900

    デバイスの表示不具合修正

--- src/common/theme/layout.tsx
+++ src/common/theme/layout.tsx
@@ -6,14 +6,13 @@ const breakpoint = 600;
 
 export const maxQueryProperty = `@media (max-width: ${breakpoint}px)`;
 
-/** pc版ブレークポイント */
-export const pcDisplayQuery = `(min-width: ${breakpoint}px)`;
+/** PC版ブレークポイント */
+export const pcDisplayQuery = `(max-width:" ${breakpoint}px)`;
 /** スマホ版ブレークポイント */
-export const spDisplayQuery = `(max-width:" ${breakpoint}px)`;
+export const spDisplayQuery = `(min-width: ${breakpoint}px)`;
 
 /** 最大幅 */
 export const maxWidthStyle = css({
     maxWidth: '1200px',
     margin: 'auto'
 });
-
