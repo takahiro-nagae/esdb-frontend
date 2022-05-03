e54dc867329ffb36a83257c6de793484a9d80f4e
takahiro-nagae <tnagae627@gmail.com>
2022-05-04 01:23:57 +0900

    データにJSDocを追加

--- src/search/ground/impl/groundEnchantImpl.tsx
+++ src/search/ground/impl/groundEnchantImpl.tsx
@@ -4,10 +4,15 @@ import {GroundEnchant} from "../interface/groundEnchant";
  * 下地エンチャントの実装クラス
  */
 export class GroundEnchantImpl implements GroundEnchant {
+    /** エンチャントID */
     enchant_id: string;
+    /** エンチャント名 */
     enchant_name: string;
+    /** エンチャント別名 */
     enchant_name_2: string;
+    /** エンチャント英名 */
     enchant_name_en: string;
+    /** 位置ID */
     position_id: string;
 
     constructor() {
--- src/search/ground/interface/ground.tsx
+++ src/search/ground/interface/ground.tsx
@@ -4,6 +4,8 @@ import {GroundEnchant} from "./groundEnchant";
  * 下地の一覧インターフェース
  */
 export interface Ground {
+    /** ランク */
     rank: string;
+    /** ランクに紐づくエンチャントの一覧 */
     enchant_list: Array<GroundEnchant>;
 }
