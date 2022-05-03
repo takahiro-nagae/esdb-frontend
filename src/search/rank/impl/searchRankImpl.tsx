029aad5fcb9f484b975f2f79a19e917810c91cf9
kumario <kumalog@jackumagic.com>
2022-05-04 01:22:13 +0900

    データにJSDocを追加

--- src/search/rank/impl/searchRankImpl.tsx
+++ src/search/rank/impl/searchRankImpl.tsx
@@ -4,16 +4,27 @@ import {searchRank} from "../interface/searchRank";
  * 検索画面のランク実装ランク
  */
 export class SearchRankImpl implements searchRank {
-    ancient_rate: string;
-    ancient_rate_thu: string;
-    elf_rate: string;
-    elf_rate_thu: string;
-    elite_rate: string;
-    elite_rate_thu: string;
-    normal_rate: string;
-    normal_rate_thu: string;
+    /** ランク */
     rank: string;
+    /** 通常：木曜以外 */
+    normal_rate: string;
+    /** エリート：木曜以外 */
+    elite_rate: string;
+    /** エルフ：木曜以外 */
+    elf_rate: string;
+    /** 古代：木曜以外 */
+    ancient_rate: string;
+    /** 稀代：木曜以外 */
     rare_holy_rate: string;
+    /** 通常：木曜 */
+    normal_rate_thu: string;
+    /** エリート：木曜 */
+    elite_rate_thu: string;
+    /** エルフ：木曜 */
+    elf_rate_thu: string;
+    /** 古代：木曜 */
+    ancient_rate_thu: string;
+    /** 稀代：木曜 */
     rare_holy_rate_thu: string;
 
     constructor() {
