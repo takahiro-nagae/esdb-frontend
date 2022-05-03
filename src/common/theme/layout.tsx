6fc0705517a9093e1364c9b13ef6a0f0a6dc4c5f
takahiro-nagae <tnagae627@gmail.com>
2022-05-03 14:01:33 +0900

    レイアウトの不具合修正

--- src/common/theme/layout.tsx
+++ src/common/theme/layout.tsx
@@ -7,9 +7,9 @@ const breakpoint = 600;
 export const maxQueryProperty = `@media (max-width: ${breakpoint}px)`;
 
 /** PC版ブレークポイント */
-export const pcDisplayQuery = `(max-width:" ${breakpoint}px)`;
-/** スマホ版ブレークポイント */
-export const spDisplayQuery = `(min-width: ${breakpoint}px)`;
+export const pcDisplayQuery = `(min-width: ${breakpoint}px)`;
+/** SP版ブレークポイント */
+export const spDisplayQuery = `(max-width:" ${breakpoint}px)`;
 
 /** 最大幅 */
 export const maxWidthStyle = css({
