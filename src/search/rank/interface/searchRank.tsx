a48db179fe19b903cdc44f98c4a021e5192566ab
kumario <kumalog@jackumagic.com>
2022-05-04 01:15:23 +0900

    ランクディレクトリのリファクタ

---
+++ src/search/rank/interface/searchRank.tsx
@@ -0,0 +1,16 @@
+/**
+ * 検索画面のランクインターフェース
+ */
+export interface searchRank {
+    rank: string;
+    normal_rate: string;
+    elite_rate: string;
+    elf_rate: string;
+    ancient_rate: string;
+    rare_holy_rate: string;
+    normal_rate_thu: string;
+    elite_rate_thu: string;
+    elf_rate_thu: string;
+    ancient_rate_thu: string;
+    rare_holy_rate_thu: string;
+}
