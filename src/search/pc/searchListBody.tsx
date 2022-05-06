d46ace2c5e97f43737c9aa97a5e132632dad26cf
takahiro-nagae <tnagae627@gmail.com>
2022-05-06 18:32:47 +0900

    PCの個別処理をコンテナに外出しする

--- src/search/pc/searchListBody.tsx
+++ src/search/pc/searchListBody.tsx
@@ -10,15 +10,16 @@ import { createEnchantName, createEnchantNameEn, subTitleStyle } from '../common
 import { GroundButton } from '../ground/groundButton';
 import { EffectList } from "../common/compornent/effectList";
 import { RouteList } from "../common/compornent/routeList";
-import {InvalidText} from "../common/compornent/invalidText";
-import {ImpText} from "../common/compornent/impText";
+import { InvalidText } from "../common/compornent/invalidText";
+import { ImpText } from "../common/compornent/impText";
+import { EnchantData } from "../common/interface/enchantData";
 
 /**
  * PCの検索一覧
  */
-export const SearchListBody = (props: {enchant: any, valFlg: boolean}) => {
+export const SearchListBody = ( props: { enchant: EnchantData, valFlg: boolean } ) => {
     /** コンテント各行の基本スタイル */
-    const tableContent = css ({
+    const tableContent = css( {
         backgroundColor: '#3C3B40',
         'td': {
             color: '#fff'
@@ -26,29 +27,32 @@ export const SearchListBody = (props: {enchant: any, valFlg: boolean}) => {
         'svg': {
             color: '#fff'
         }
-    });
+    } );
 
-    return(
-        <TableRow css={tableContent}>
-            {/* 名称 */}
+    return (
+        <TableRow css={ tableContent }>
+            {/* 名称 */ }
             <TableCell>
-                <span>{createEnchantName(props.enchant.enchant_name, props.enchant.enchant_name_2)}</span>
-                <InvalidText  invalidTargetFlg={props.enchant.invalid_target_flg} />
-                <ImpText impFlg={ props.enchant.imp_flg } />
-                <br />
-                <small css={subTitleStyle}>{createEnchantNameEn(props.enchant.enchant_name_en, props.enchant.position_id)}</small>
+                <span>{ createEnchantName( props.enchant.enchant_name, props.enchant.enchant_name_2 ) }</span>
+                <InvalidText invalidTargetFlg={ props.enchant.invalid_target_flg }/>
+                <ImpText impFlg={ props.enchant.imp_flg }/>
+                <br/>
+                <small
+                    css={ subTitleStyle }>{ createEnchantNameEn( props.enchant.enchant_name_en, props.enchant.position_id ) }</small>
             </TableCell>
-            {/* 位置 */}
-            <TableCell css={positionColor(props.enchant.position_id)} >{positionName(props.enchant.position_id)}</TableCell>
-            {/* ランク */}
+            {/* 位置 */ }
+            <TableCell
+                css={ positionColor( props.enchant.position_id ) }>{ positionName( props.enchant.position_id ) }</TableCell>
+            {/* ランク */ }
             <TableCell>
-                <RankModal rank={props.enchant.rank} /><br />
-                <GroundButton enchant_id={props.enchant.enchant_id} rank_ignore_flg={props.enchant.rank_ignore_flg} rank_seq={props.enchant.rank_seq} />
+                <RankModal rank={ props.enchant.rank }/><br/>
+                <GroundButton enchant_id={ props.enchant.enchant_id } rank_ignore_flg={ props.enchant.rank_ignore_flg }
+                              rank_seq={ props.enchant.rank_seq }/>
             </TableCell>
-            {/* 対象 */}
-            <TableCell>{props.enchant.target_name}</TableCell>
-            {/* 値 */}
-            {props.valFlg && <TableCell>{props.enchant.disp_val}</TableCell>}
+            {/* 対象 */ }
+            <TableCell>{ props.enchant.target_name }</TableCell>
+            {/* 値 */ }
+            { props.valFlg && <TableCell>{ props.enchant.disp_val }</TableCell> }
             <TableCell>
                 <EffectList
                     effectKbn={ props.enchant.effect_kbn }
