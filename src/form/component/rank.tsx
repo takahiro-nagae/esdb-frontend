8bea1b45dbdcc1ff9a293d60c9513e0fdbe266bc
takahiro-nagae <tnagae627@gmail.com>
2022-05-08 22:30:46 +0900

    onChange共通化はコストがかかるので断念

--- src/form/component/rank.tsx
+++ src/form/component/rank.tsx
@@ -12,80 +12,79 @@ import { Dispatch, SetStateAction } from "react";
  * @param props { Array<RankType>, string, UseFormRegister<FormType>, Dispatch<SetStateAction<string>> }
  * @returns Rank { EmotionJSX.Element }
  */
-export const Rank = (props: {
+export const Rank = ( props: {
     rankList: Array<RankType>,
     rankRange: string,
     register: UseFormRegister<FormType>
     setRankRange: Dispatch<SetStateAction<string>>,
-}) => {
+} ) => {
 
     /** ランクの現在値変更 */
-    // TODO: formのonChangeを共通化する
     const onChange = (
         _event: React.MouseEvent<HTMLElement>,
         newRankValue: string | null,
-        ) => {
-        if (newRankValue !== null) {
-            props.setRankRange(newRankValue);
+    ) => {
+        if ( newRankValue !== null ) {
+            props.setRankRange( newRankValue );
         }
     };
 
-    return(
+    return (
         <>
-           <label css={labelStyle} ><small>ランク</small></label>
-            <Grid container css={formMarginStyle}>
-                {/* ランクセレクトボックス */}
-                <Grid item xs={4}>
+            <label css={ labelStyle }><small>ランク</small></label>
+            <Grid container css={ formMarginStyle }>
+                {/* ランクセレクトボックス */ }
+                <Grid item xs={ 4 }>
                     <TextField
-                        css={formMarginStyle}
+                        css={ formMarginStyle }
                         defaultValue=''
                         fullWidth
                         id='rank'
                         select
                         size='small'
                         variant='outlined'
-                        {...props.register('rank')}
+                        { ...props.register( 'rank' ) }
                     >
                         <MenuItem value=''>指定無し</MenuItem>
-                        {props.rankList.map(rank => (
+                        { props.rankList.map( rank => (
                             <MenuItem
-                                key={rank['rank']}
-                                value={rank['rank']}
+                                key={ rank['rank'] }
+                                value={ rank['rank'] }
                             >
-                                {rank['rank']}
+                                { rank['rank'] }
                             </MenuItem>
-                        ))}
+                        ) ) }
                     </TextField>
                 </Grid>
-                {/* 余白 */}
-                <Grid item xs={1} />
-                {/* ランク一致 */}
-                <Grid item xs={7}>
+                {/* 余白 */ }
+                <Grid item xs={ 1 }/>
+                {/* ランク一致 */ }
+                <Grid item xs={ 7 }>
                     <ToggleButtonGroup
-                        css={formMarginStyle}
+                        css={ formMarginStyle }
                         fullWidth
                         exclusive
                         size='small'
-                        onChange={onChange}
-                        value={props.rankRange}
+                        onChange={ onChange }
+                        value={ props.rankRange }
                     >
                         <ToggleButton
                             aria-label='1'
-                            css={selectBoxStyle}
+                            css={ selectBoxStyle }
                             value='1'
                         >
                             <span>一致</span>
                         </ToggleButton>
                         <ToggleButton
                             aria-label='2'
-                            css={selectBoxStyle}
+                            css={ selectBoxStyle }
                             value='2'
                         >
                             <span>以上</span>
                         </ToggleButton>
                         <ToggleButton
                             aria-label='3'
-                            css={selectBoxStyle}
+                            css={ selectBoxStyle }
                             value='3'
                         >
                             <span>以下</span>
