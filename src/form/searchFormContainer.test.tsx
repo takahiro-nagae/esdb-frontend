0a89ae2f35f4e9c5d0660ae9e265610985b7b5ee
kumario <kumalog@jackumagic.com>
2022-05-25 01:18:24 +0900

    テスト作成（warningでるけどとりあえずプッシュ）

--- src/form/searchFormContainer.test.tsx
+++ src/form/searchFormContainer.test.tsx
@@ -2,8 +2,48 @@ import React from "react";
 import TestRenderer from 'react-test-renderer';
 import { SearchFormContainer } from "./searchFormContainer";
 import { BrowserRouter } from "react-router-dom";
+import { screen } from "@testing-library/react";
+import renderComponent from "../tesetLib/render";
+import userEvent from "@testing-library/user-event";
 
-it( 'snapshot test', () => {
-    const ss = TestRenderer.create( <BrowserRouter><SearchFormContainer/></BrowserRouter> ).toJSON();
-    expect( ss ).toMatchSnapshot();
-} );
+test('snapshot test', () => {
+    const ss = TestRenderer.create(<BrowserRouter><SearchFormContainer/></BrowserRouter>).toJSON();
+    expect(ss).toMatchSnapshot();
+});
+
+const rendering = () => {
+    renderComponent(<BrowserRouter><SearchFormContainer/></BrowserRouter>);
+};
+
+/**
+ * @param labelText { string }
+ * @param expected { string }
+ */
+const testingInputInitialValForGetByLabelText = (labelText: string, expected: string) => {
+    const input: HTMLInputElement = screen.getByLabelText(labelText);
+    expect(input.value).toBe(expected);
+}
+
+/**
+ * @param labelText { string }
+ * @param types { string }
+ * @param expected { string }
+ */
+const testingInputValForGetByLabelText = async (labelText: string, types: string, expected: string) => {
+    const input: HTMLInputElement = screen.getByLabelText(labelText);
+    userEvent.type(input, types);
+    expect(input.value).toBe(expected);
+}
+
+test('エンチャント名', () => {
+    testingInputInitialValForGetByLabelText('エンチャント名', '');
+
+});
+
+test('aa', () => {
+    testingInputValForGetByLabelText(
+        'エンチャント名',
+        '1１@＃Qdあイｳ江🌍',
+        '1１@＃Qdあイｳ江🌍'
+    );
+});
