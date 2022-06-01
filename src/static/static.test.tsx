90b57893330fd740c3c1609031ce4031fb6999c9
kumario <kumalog@jackumagic.com>
2022-06-01 10:29:30 +0900

    静的ページ系のスナップショットを追加

---
+++ src/static/static.test.tsx
@@ -0,0 +1,24 @@
+import TestRenderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { About } from "./about";
+import { AppHistory } from "./appHistory";
+import { PrivacyPolicy } from "./privacyPolicy";
+
+describe('snapshot test', () => {
+    test('about', () => {
+        const ss = TestRenderer.create(<BrowserRouter><About/></BrowserRouter>).toJSON();
+        expect(ss).toMatchSnapshot();
+    });
+
+    test('appHistory', () => {
+        const ss = TestRenderer.create(<BrowserRouter><AppHistory/></BrowserRouter>).toJSON();
+        expect(ss).toMatchSnapshot();
+    });
+
+    test('privacyPolicy', () => {
+        const ss = TestRenderer.create(<BrowserRouter><PrivacyPolicy/></BrowserRouter>).toJSON();
+        expect(ss).toMatchSnapshot();
+    });
+});
+
