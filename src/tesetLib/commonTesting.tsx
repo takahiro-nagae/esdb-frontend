f247d9a09dba787c45080d7f925b91e2d189ce32
takahiro-nagae <tnagae627@gmail.com>
2022-06-06 09:08:07 +0900

    不要なテスト関数削除

--- src/tesetLib/commonTesting.tsx
+++ src/tesetLib/commonTesting.tsx
@@ -2,16 +2,6 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 /**
- * ラベルに紐づく要素の初期値を確認
- * @param labelText { string }
- * @param expected { string }
- */
-export const testingInputInitialValForGetByLabelText = (labelText: string, expected: string) => {
-    const input: HTMLInputElement = screen.getByLabelText(labelText);
-    expect(input.value).toBe(expected);
-}
-
-/**
  * @param labelText { string }
  * @param types { string }
  * @param expected { string }
