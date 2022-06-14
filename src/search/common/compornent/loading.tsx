3ba476a9987b7b342d1a406a616e122626c60cc1
kumario <kumalog@jackumagic.com>
2022-06-14 10:28:01 +0900

    loadingのテストを追加

--- src/search/common/compornent/loading.tsx
+++ src/search/common/compornent/loading.tsx
@@ -7,34 +7,37 @@ import { css } from '@emotion/react';
  * @param props { boolean, string? }
  * @returns Loading { JSX.Element }
  */
-export const Loading = ( props: {
+export const Loading = (props: {
     isLoading: boolean,
     backGroundColorStr?: string
-} ) => {
+}) => {
 
     const defaultColor = '#27292D';
 
     /** ローディング外周の要素 */
-    const loadingContainerStyle = css( {
+    const loadingContainerStyle = css({
         backgroundColor: props.backGroundColorStr ? props.backGroundColorStr : defaultColor,
         height: '100%',
         width: '100%',
         position: 'fixed',
         zIndex: 999
-    } );
+    });
 
     /** ローディングの表示位置 */
-    const verticalCenterStyle = css( {
+    const verticalCenterStyle = css({
         position: 'absolute',
         left: '50%',
         top: '50%',
-    } );
+    });
 
     return (
         <>
-            { !props.isLoading &&
-                <div css={ loadingContainerStyle }>
-                    <ReactLoading css={ verticalCenterStyle } type="bubbles"/>
+            {!props.isLoading &&
+                <div
+                    css={loadingContainerStyle}
+                    data-testid='loading'
+                >
+                    <ReactLoading css={verticalCenterStyle} type="bubbles"/>
                 </div>
 
             }
