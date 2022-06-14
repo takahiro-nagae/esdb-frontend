d65ad05461a9a3a427287b3a57d6ff3342c47916
takahiro-nagae <tnagae627@gmail.com>
2022-06-14 09:37:59 +0900

    effectListのtestを追加

---
+++ src/search/common/compornent/effectList.test.tsx
@@ -0,0 +1,31 @@
+import renderComponent from "../../../tesetLib/render";
+import React from "react";
+import { EffectList } from "./effectList";
+import { screen } from "@testing-library/react";
+
+const rendering = (effectKbn: string, effectName: string) => {
+    renderComponent(<EffectList effectKbn={effectKbn} effectName={effectName}/>);
+}
+
+describe('effectList', () => {
+    test('値が全て空', () => {
+        rendering('', '');
+
+        expect(screen.queryByTestId('effect')).toBeNull();
+    });
+
+    test('値を渡した時の表示及び表示順確認', async () => {
+        // 本当はここで色も確認したいが複雑になるのでテストを分けた
+        // 色は関数の単体テストeffectColorFunction.test.tsにて実施
+        const effectKbn = 'increase@decrease@not-relevant@designated@';
+        const effectName = 'test0@test1@test2@test3@test4';
+
+        rendering(effectKbn, effectName);
+
+        const effectNameArray = effectName.split('@');
+
+        screen.getAllByTestId('effect').map((screenEffect, index) => {
+            expect(screenEffect.textContent).toBe(effectNameArray[index]);
+        });
+    });
+});
