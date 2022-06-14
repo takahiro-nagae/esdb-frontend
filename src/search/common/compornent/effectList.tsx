b55635890580297185ec6d7143530b2005e34f4c
kumario <kumalog@jackumagic.com>
2022-06-14 09:37:59 +0900

    effectListのtestを追加

--- src/search/common/compornent/effectList.tsx
+++ src/search/common/compornent/effectList.tsx
@@ -11,20 +11,21 @@ export const EffectList = (props: {
     effectName: string
 }) => {
     /** 効果区分を配列化 */
-    const effectKbnArray:"" | string[] = props.effectKbn && props.effectKbn.split('@');
+    const effectKbnArray: "" | string[] = props.effectKbn && props.effectKbn.split('@');
     /** 効果名を配列化 */
-    const effectNameArray:"" | string[] = props.effectName && props.effectName.split('@');
+    const effectNameArray: "" | string[] = props.effectName && props.effectName.split('@');
 
-    return(
-      <>
-          { effectKbnArray && effectKbnArray.map((effectKbn, index) =>
-              <p
-                  css={ effectColorFunction(effectKbn) }
-                  key={ index }
-              >
-                  { effectNameArray[index] }
-              </p>
-          )}
-      </>
+    return (
+        <>
+            {effectKbnArray && effectKbnArray.map((effectKbn, index) =>
+                <p
+                    css={effectColorFunction(effectKbn)}
+                    data-testid='effect'
+                    key={index}
+                >
+                    {effectNameArray[index]}
+                </p>
+            )}
+        </>
     );
 }
