82593b7619bdfe9c7d92ec57a453b116ecb96200
kumario <kumalog@jackumagic.com>
2022-06-16 20:06:45 +0900

    valflgを削除

--- src/search/pc/searchListRow.tsx
+++ src/search/pc/searchListRow.tsx
@@ -16,12 +16,12 @@ import { EnchantData } from "../common/interface/enchantData";
 
 /**
  * PCの検索一覧
- * @param props { EnchantData, boolean }
+ * @param props { EnchantData }
  * @returns SearchListRow { JSX.Element }
  */
-export const SearchListRow = ( props: { enchant: EnchantData, valFlg: boolean } ) => {
+export const SearchListRow = (props: { enchant: EnchantData }) => {
     /** コンテント各行の基本スタイル */
-    const tableContent = css( {
+    const tableContent = css({
         backgroundColor: '#3C3B40',
         'td': {
             color: '#fff'
@@ -29,46 +29,46 @@ export const SearchListRow = ( props: { enchant: EnchantData, valFlg: boolean } 
         'svg': {
             color: '#fff'
         }
-    } );
+    });
 
     return (
-        <TableRow css={ tableContent }>
+        <TableRow css={tableContent}>
             <TableCell>
-                <span>{ createEnchantName( props.enchant.enchant_name, props.enchant.enchant_name_2 ) }</span>
-                <InvalidText invalidTargetFlg={ props.enchant.invalid_target_flg }/>
-                <ImpText impFlg={ props.enchant.imp_flg }/>
+                <span>{createEnchantName(props.enchant.enchant_name, props.enchant.enchant_name_2)}</span>
+                <InvalidText invalidTargetFlg={props.enchant.invalid_target_flg}/>
+                <ImpText impFlg={props.enchant.imp_flg}/>
                 <br/>
-                <small css={ subTitleStyle }>
-                    { createEnchantNameEn( props.enchant.enchant_name_en, props.enchant.position_id ) }
+                <small css={subTitleStyle}>
+                    {createEnchantNameEn(props.enchant.enchant_name_en, props.enchant.position_id)}
                 </small>
             </TableCell>
-            <TableCell css={ positionColor( props.enchant.position_id ) }>
-                { positionName( props.enchant.position_id ) }
+            <TableCell css={positionColor(props.enchant.position_id)}>
+                {positionName(props.enchant.position_id)}
             </TableCell>
             <TableCell>
-                <RankModal rank={ props.enchant.rank }/>
+                <RankModal rank={props.enchant.rank}/>
                 <br/>
                 <GroundButton
-                    enchant_id={ props.enchant.enchant_id }
-                    rank_ignore_flg={ props.enchant.rank_ignore_flg }
-                    rank_seq={ props.enchant.rank_seq }
+                    enchant_id={props.enchant.enchant_id}
+                    rank_ignore_flg={props.enchant.rank_ignore_flg}
+                    rank_seq={props.enchant.rank_seq}
                 />
             </TableCell>
-            <TableCell>{ props.enchant.target_name }</TableCell>
+            <TableCell>{props.enchant.target_name}</TableCell>
             {
-                props.valFlg &&
-                <TableCell>{ props.enchant.disp_val }</TableCell>
+                props.enchant.disp_val &&
+                <TableCell>{props.enchant.disp_val}</TableCell>
             }
             <TableCell>
                 <EffectList
-                    effectKbn={ props.enchant.effect_kbn }
-                    effectName={ props.enchant.effect_name }
+                    effectKbn={props.enchant.effect_kbn}
+                    effectName={props.enchant.effect_name}
                 />
             </TableCell>
             <TableCell>
                 <RouteList
-                    enchantId={ props.enchant.enchant_id }
-                    routeName={ props.enchant.route_name }
+                    enchantId={props.enchant.enchant_id}
+                    routeName={props.enchant.route_name}
                 />
             </TableCell>
         </TableRow>
