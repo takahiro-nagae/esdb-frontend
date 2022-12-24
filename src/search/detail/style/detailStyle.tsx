6b222185f8e46ee1591c64297705d61731ceb11e
takahiro-nagae <tnagae627@gmail.com>
2022-12-25 02:33:03 +0900

    detail配下cssを外部化

---
+++ src/search/detail/style/detailStyle.tsx
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const header = css({
+    color: '#fff',
+    backgroundColor: '#2f2f2f',
+    borderBottom: '1px solid rgba(81, 81, 81, 1)'
+});
+
+export const body = css({
+    color: '#fff',
+    backgroundColor: '#3C3B40',
+    borderBottom: '1px solid rgba(81, 81, 81, 1)'
+});
