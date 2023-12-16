254372cb8fc37c2beb9126a5e2a08571dda23b47
takahiro-nagae <tnagae627@gmail.com>
2023-12-16 14:09:14 +0900

    imptextをリファクタ

--- src/features/search/common/compornent/ImpText/ImpText.tsx
+++ src/features/search/common/compornent/ImpText/ImpText.tsx
@@ -1,18 +1,12 @@
-import { css } from "@emotion/react";
-
 /** @jsxImportSource @emotion/react */
+import { notImped } from "./style/ImptextStyle";
 
 export const ImpText = (props: { impFlg: string }) => {
-    const red = css({
-        color: '#f00',
-        fontWeight: 'bold'
-    });
-
     return (
         <>
             {
                 props.impFlg == '0' &&
-                <small css={red}>
+                <small css={notImped}>
                     未実装
                 </small>
             }
---
+++ src/features/search/common/compornent/ImpText/stories/ImpText.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ImpText } from "../ImpText";
+
+export default {
+    title: 'search/common/component/ImpText',
+    component: ImpText,
+} as Meta<typeof ImpText>;
+
+export const NotImped: StoryObj<typeof ImpText> = {
+    args: {
+        impFlg: '0',
+    }
+};
+
+export const Imped: StoryObj<typeof ImpText> = {
+    args: {
+        impFlg: '1',
+    }
+};
