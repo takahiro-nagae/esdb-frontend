b7e1cc1af8e25bdddd8cf0cf6edfe22a2102991e
takahiro-nagae <tnagae627@gmail.com>
2024-01-04 13:17:46 +0900

    searchResultをリファクタ

--- src/features/search/searchListStyle.tsx
+++ src/features/search/searchListStyle.tsx
@@ -1,16 +1,5 @@
 import { css } from '@emotion/react';
 
-export const result = css({
-    color: '#fff',
-    fontWeight: 'bold',
-    textAlign: 'center',
-});
-
-export const hitCount = css({
-    color: '#f00',
-    fontSize: '18px',
-});
-
 export const verticalCenter = css({
     position: 'absolute',
     top: '50%',
