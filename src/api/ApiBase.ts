2953b5a46be06f3a1097351b3b4723d8d217975c
takahiro-nagae <tnagae627@gmail.com>
2024-06-02 18:48:11 +0900

    広告をtsxに
    テストを通るように

--- src/api/ApiBase.ts
+++ src/api/ApiBase.ts
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 
 const settingEndpoint = () => {
-    switch (import.meta.env.VITE_APP_ENV) {
+    switch (process.env.VITE_APP_ENV) {
         case 'dev':
             axios.defaults.baseURL = 'http://localhost:5001';
             break;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import env from "vite-plugin-env-compatible";
 
 export default ({ mode }) => {
     return defineConfig({
       plugins: [
         react(),
+        env({ prefix: "VITE",  mountedPath: "process.env" })
       ],
       esbuild: {
         jsxInject: `import React from 'react';`,
