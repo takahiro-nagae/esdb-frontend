52e5f765dd9c76783e8a84e0bebb14566e1d9f41
takahiro-nagae <tnagae627@gmail.com>
2024-06-02 17:25:02 +0900

    webpackの設定ファイル除去

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,10 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
     framework: {
-        name: '@storybook/react-webpack5',
+        name: '@storybook/react-vite',
         options: {},
     },
     docs: {
