a1fe5fab52c75ec1149cf3a0bccbb28f3ce5cd12
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-14 00:24:44 +0900

    不要なパッケージの削除

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    'storybook-addon-mock/register',
   ],
   framework: {
     name: '@storybook/react-vite',
