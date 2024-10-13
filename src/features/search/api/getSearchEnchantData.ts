af919a95e00d5821197cd2c0df7c66f71c57c3d3
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-13 23:48:54 +0900

    検索結果を出るように

--- src/features/search/api/getSearchEnchantData.ts
+++ src/features/search/api/getSearchEnchantData.ts
@@ -1,27 +1,23 @@
-import { internalApiClient } from "@/repositories/_client";
-import { EnchantData } from "../common/interface/enchantData";
+import { internalApiClient } from '@/repositories/_client';
+import { EnchantData } from '../common/interface/enchantData';
 
 type SearchInfo = {
-    enchantList: Array<EnchantData>;
-    effectName: string;
-}
+  enchantList: Array<EnchantData>;
+  effectName: string;
+};
 
-export const getSearchEnchantData = async(path: string, requestParams: URLSearchParams) => {
-    const response: SearchInfo = {
-        enchantList: [],
-        effectName: '',
-    };
-    await internalApiClient(path + '?' + requestParams)
-        .then(res => {
-            if (res.data) {
-                response.enchantList = res.data.enchant_list;
-                if (res.data.effect_name) {
-                    response.effectName = res.data.effect_name.effect;
-                }
-            }
-        }).catch(error => {
-            console.log(error);
-        });
-
-    return response;
+export const getSearchEnchantData = async (
+  path: string,
+  requestParams: URLSearchParams,
+) => {
+  const response: SearchInfo = {
+    enchantList: [],
+    effectName: '',
+  };
+  const data = await internalApiClient(path + '?' + requestParams);
+  response.enchantList = data.enchant_list;
+  if (data.effect_name) {
+    response.effectName = data.effect_name.effect;
+  }
+  return response;
 };
