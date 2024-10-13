ccf30dcceddda98b892e4b369ae62f2d6b9c7596
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-14 01:46:22 +0900

    buildエラー解決

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { MultiplexAd } from '../../adsense/multiplexAd';
 import { SearchFormContainer } from '../form/SearchFormContainer';
 
-/**
- * トップ画面コンポーネント
- * @returns Home { JSX.Element }
- */
 export const Home: React.FC = () => {
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,18 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import env from 'vite-plugin-env-compatible';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [react(), env({ prefix: 'VITE', mountedPath: 'process.env' })],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    },
-    esbuild: {
-      jsxInject: `import React from 'react';`,
-    },
-    build: {
-      sourcemap: mode !== 'prod' ? true : false,
-    },
-    server: {
-      port: 3000,
-    },
-  });
-};
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    env({ prefix: 'VITE', mountedPath: 'process.env' }),
+    tsconfigPaths(),
+  ],
+  build: {
+    sourcemap: mode !== 'prod' ? true : false,
+  },
+  server: {
+    port: 3000,
+  },
+}));
