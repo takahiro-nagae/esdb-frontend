9b0648e1f5589d4379831bcdc7f67862b4665e6a
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-14 16:56:02 +0900

    schema修正

--- src/repositories/search/fetchRankData.ts
+++ src/repositories/search/fetchRankData.ts
@@ -1,20 +1,7 @@
-import { z } from 'zod';
 import { internalApiClient } from '../_client';
 import { RankDataSchema } from './_schema';
 
-const ResponseSchema = z.object({
-  rank: RankDataSchema.shape.rank,
-  normal_rate: RankDataSchema.shape.normal_rate.optional(),
-  elite_rate: RankDataSchema.shape.elite_rate.optional(),
-  elf_rate: RankDataSchema.shape.elf_rate.optional(),
-  ancient_rate: RankDataSchema.shape.ancient_rate.optional(),
-  rare_holy_rate: RankDataSchema.shape.rare_holy_rate.optional(),
-  normal_rate_thu: RankDataSchema.shape.normal_rate_thu.optional(),
-  elite_rate_thu: RankDataSchema.shape.elite_rate_thu.optional(),
-  elf_rate_thu: RankDataSchema.shape.elf_rate_thu.optional(),
-  ancient_rate_thu: RankDataSchema.shape.ancient_rate_thu.optional(),
-  rare_holy_rate_thu: RankDataSchema.shape.rare_holy_rate_thu.optional(),
-});
+const ResponseSchema = RankDataSchema;
 
 export const fetchRankData = async (rank: string) => {
   const data = await internalApiClient('rank/' + rank);
