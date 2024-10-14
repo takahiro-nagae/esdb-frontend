de0445c6c59e60fd40b8198a98057757cd6a84d8
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-14 16:54:37 +0900

    件数のリファクタ

--- src/features/search/components/SearchResult/SearechResultHead.tsx
+++ src/features/search/components/SearchResult/SearechResultHead.tsx
@@ -1,31 +1,30 @@
-/** @jsxImportSource @emotion/react */
-import { hitCount, result } from "./style/SearechResultHeadStyle";
+import styles from './SearchResultHead.module.css';
 
 type SearchResultType = {
-    dispCount: number;
-    count: number;
-    effectName: string;
+  dispCount: number;
+  count: number;
+  effectName: string;
 };
 
-const Compornent = (props: SearchResultType) => {
-    return (
-        <>
-            {props.dispCount < 1 && (
-                <p css={result}>検索結果は0件です</p>
-            )}
-            {props.dispCount >= 1 && (
-                <p css={result}>
-                    <span css={hitCount}>{props.count}</span>件ヒットしました
-                    {props.effectName != '' && (
-                        <>
-                            <br />
-                            <span>値：{props.effectName}</span>
-                        </>
-                    )}
-                </p>
-            )}
-        </>
-    );
+export const SearchResultHead: React.FC<SearchResultType> = ({
+  dispCount,
+  count,
+  effectName,
+}) => {
+  return (
+    <>
+      {dispCount < 1 && <p className={styles.result}>検索結果は0件です</p>}
+      {dispCount >= 1 && (
+        <p className={styles.result}>
+          <span className={styles.hitCount}>{count}</span>件ヒットしました
+          {effectName != '' && (
+            <>
+              <br />
+              <span>値：{effectName}</span>
+            </>
+          )}
+        </p>
+      )}
+    </>
+  );
 };
-
-export const SearchResultHead = Compornent;
