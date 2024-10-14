3e2d4dbe5a0cdafe73ae69a3e90b882d5f9b42b7
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-14 13:53:36 +0900

    searchFilterのリファクタ

--- src/features/search/common/component/SearchFilter/SearchFilter.tsx
+++ src/features/search/common/component/SearchFilter/SearchFilter.tsx
@@ -1,24 +1,31 @@
-/* eslint @typescript-eslint/no-explicit-any: 0 */
-/** @jsxImportSource @emotion/react */
-import { useState } from 'react';
-import { Grid } from "@material-ui/core";
-import { GridSize } from "@material-ui/core/Grid/Grid";
-import { useSearchFilter } from "./hooks/useSearchFilter";
-import { freeSearchBoxStyle, freeSearchInputStyle } from "./style/SearchFilterStyle";
+import { Grid } from '@material-ui/core';
+import { GridSize } from '@material-ui/core/Grid/Grid';
+import { useSearchFilter } from './hooks/useSearchFilter';
+import styles from './SearchFilter.module.css';
+import { isBrowser } from 'react-device-detect';
 
-export const SearchFilter = (props: {
-    xs: GridSize,
-}) => {
-    const [searchWord, setSearchWord]  = useState('');
-    useSearchFilter(searchWord);
+export const SearchFilter = (props: { xs: GridSize }) => {
+  const { searchWord, setSearchWord } = useSearchFilter();
 
-    return (
-        <Grid item xs={props.xs} css={freeSearchBoxStyle}>
-            <input css={freeSearchInputStyle}
-                   placeholder='絞り込む'
-                   value={searchWord}
-                   data-testid='searchFilter'
-                   onChange={(e) => setSearchWord(e.target.value)}/>
-        </Grid>
-    );
+  return (
+    <Grid
+      item
+      xs={props.xs}
+      className={`${styles.freeSearchBox} ${
+        isBrowser ? styles.freeSearchBoxBrowser : styles.freeSearchBoxMobile
+      }`}
+    >
+      <input
+        className={`${styles.freeSearchInput} ${
+          isBrowser
+            ? styles.freeSearchInputBrowser
+            : styles.freeSearchInputMobile
+        }`}
+        placeholder='絞り込む'
+        value={searchWord}
+        data-testid='searchFilter'
+        onChange={e => setSearchWord(e.target.value)}
+      />
+    </Grid>
+  );
 };
