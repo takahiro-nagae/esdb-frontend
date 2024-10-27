574fdfd94f408dee5d88efe113974ed600708e38
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-27 15:24:32 +0900

    commonのstories追加

---
+++ src/common/stories/Footer.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { Footer } from '../Footer';
+
+export default {
+  title: 'common/Footer',
+  component: Footer,
+  decorators: [
+    Story => {
+      return <Story />;
+    },
+  ],
+} as Meta<typeof Footer>;
+
+const Template: StoryFn<typeof Footer> = () => {
+  return <Footer />;
+};
+
+export const Default = Template.bind({});
