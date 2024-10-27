df40ab4f9d82bfdb9bcbaeebb329dffb632bfad7
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-10-27 10:46:11 +0900

    src直下の修正

--- src/tesetLib/render.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint @typescript-eslint/no-explicit-any: 0 */
-import { render } from '@testing-library/react';
-import React from 'react';
-
-/** コンポーネントレンダリング */
-export default function renderComponent(el: React.ReactElement<any, string | React.JSXElementConstructor<any>>) {
-    return render(el);
-}
