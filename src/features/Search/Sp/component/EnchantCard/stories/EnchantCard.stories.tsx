e029d07625289e737bd70c9bb3eb1665bfef1bc5
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-11-03 19:41:15 +0900

    fix test

--- src/features/Search/Sp/component/EnchantCard/stories/EnchantCard.stories.tsx
+++ src/features/Search/Sp/component/EnchantCard/stories/EnchantCard.stories.tsx
@@ -20,7 +20,7 @@ export const AllView: StoryObj<typeof EnchantCard> = {
     enchant: {
       ...ENCHANT_DATA_MOCK,
       imp_flg: '0',
-      invalid_target_flg: 1,
+      invalid_target_flg: '1',
       disp_val: 100,
     },
   },
