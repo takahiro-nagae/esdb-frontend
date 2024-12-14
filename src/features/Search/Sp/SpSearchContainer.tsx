68309272258851d6f7669b7109c336b60764fba8
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2024-12-14 20:02:56 +0900

    ブックマーク追加機能実装

--- src/features/Search/Sp/SpSearchContainer.tsx
+++ src/features/Search/Sp/SpSearchContainer.tsx
@@ -10,18 +10,20 @@ import { EnchantCard } from './component/EnchantCard/EnchantCard';
 import { ScrollTopButton } from './component/ScrollTopButton/ScrollTopButton';
 
 export const SpSearchContainer: React.FC = () => {
-  const { enchants, enchantsLength } = useEnchantStore();
+  const { enchants } = useEnchantStore();
   return (
     <>
       <Grid className={styles.dataWidth} item xs={12}>
         <Box sx={{ p: 1 }}>
           {enchants.map((enchant, index) => (
-            <>
+            <div key={enchant.enchant_id}>
               <AmongAd index={index} />
-              <EnchantCard enchant={enchant} key={enchant.enchant_id} />
-              <LastInfeedAd index={index} dataLength={enchantsLength} />
-            </>
+              <EnchantCard enchant={enchant} />
+            </div>
           ))}
+        </Box>
+        <Box sx={{ p: 1 }}>
+          <LastInfeedAd />
         </Box>
       </Grid>
       <ScrollTopButton />
