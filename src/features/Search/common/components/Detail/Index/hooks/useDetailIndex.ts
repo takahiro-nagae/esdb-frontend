33d5e31053513b336741bd76811342359443f969
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2025-03-15 17:31:26 +0900

    enchantListのswr化

--- src/features/Search/common/components/Detail/Index/hooks/useDetailIndex.ts
+++ src/features/Search/common/components/Detail/Index/hooks/useDetailIndex.ts
@@ -11,10 +11,11 @@ export const useDetailIndex = () => {
   const [enchantData, setEnchantData] = useState<EnchantDataDetail>();
 
   useEffect(() => {
+    setIsLoading(true);
     const enchantIdParam = params.enchant_id ? params.enchant_id : '';
     fetchEnchantDetailData(enchantIdParam).then(res => {
       setEnchantData(res);
-      setIsLoading(true);
+      setIsLoading(false);
     });
   }, []);
 
