b66947c2a70d5a5ea08c02d9599e5b7254ebefe1
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2025-03-15 17:35:59 +0900

    detailのswr化

--- src/features/Search/common/components/Detail/Index/hooks/useDetailIndex.ts
+++ src/features/Search/common/components/Detail/Index/hooks/useDetailIndex.ts
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useSWR from 'swr';
 
+import { CACHE_TIME_24H } from '@/const/cache';
 import { EnchantDataDetail } from '@/repositories/search/_types';
 import { fetchEnchantDetailData } from '@/repositories/search/fetchEnchantDetailData';
 
 export const useDetailIndex = () => {
   const params = useParams();
+  const enchantIdParam = params.enchant_id ?? '';
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [enchantData, setEnchantData] = useState<EnchantDataDetail>();
-
-  useEffect(() => {
-    setIsLoading(true);
-    const enchantIdParam = params.enchant_id ? params.enchant_id : '';
-    fetchEnchantDetailData(enchantIdParam).then(res => {
-      setEnchantData(res);
-      setIsLoading(false);
-    });
-  }, []);
+  const { data: enchantData, isLoading } = useSWR<EnchantDataDetail>(
+    ['enchantDetail', enchantIdParam],
+    () => fetchEnchantDetailData(enchantIdParam),
+    {
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      dedupingInterval: CACHE_TIME_24H,
+    },
+  );
 
   return { enchantData, isLoading };
 };
