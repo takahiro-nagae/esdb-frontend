4419ccd6e781d67ea6ca265557b096ea0eca18db
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2025-03-20 23:15:55 +0900

    常にキャッシュがあればキャッシュを参照するようにする

--- src/repositories/lib/apollo.ts
+++ src/repositories/lib/apollo.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-import { CACHE_TIME_24H } from '@/const/cache';
-
 const httpLink = createHttpLink({
   uri: 'http://localhost:3080/query',
   credentials: 'include',
@@ -9,35 +7,5 @@ const httpLink = createHttpLink({
 
 export const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          yourQueryField: {
-            keyArgs: ['id', 'filter', 'sort'],
-            merge: true,
-            read(existing: { __typename?: { cacheTime: number } } | null) {
-              if (!existing) return existing;
-
-              const cacheTime = existing.__typename
-                ? existing.__typename.cacheTime
-                : null;
-              if (cacheTime && Date.now() - cacheTime > CACHE_TIME_24H) {
-                return undefined;
-              }
-              return existing;
-            },
-          },
-        },
-      },
-    },
-  }),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-    },
-    query: {
-      fetchPolicy: 'cache-first',
-    },
-  },
+  cache: new InMemoryCache(),
 });
