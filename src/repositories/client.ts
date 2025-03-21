126141ea270bdb8f350ac2a71b244d5b26615710
takahiro-nagae <takahiro-nagae@cybozu.co.jp>
2025-03-21 20:52:22 +0900

    クライアントをprodで使えるようにした

--- src/repositories/client.ts
+++ src/repositories/client.ts
@@ -1,7 +1,25 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+const settingEndpoint = () => {
+  let uri = '';
+  switch (process.env.VITE_APP_ENV) {
+    case 'dev':
+      uri = 'http://localhost:3080';
+      break;
+    case 'stg':
+      uri = 'https://n635e4bs85.execute-api.ap-northeast-1.amazonaws.com/stage';
+      break;
+    case 'prod':
+      uri = 'https://vugeri8p1m.execute-api.ap-northeast-1.amazonaws.com/prod';
+      break;
+    default:
+      uri = 'http://localhost:3080';
+  }
+  return uri;
+};
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3080/query',
+  uri: `${settingEndpoint()}/query`,
   credentials: 'include',
 });
 
